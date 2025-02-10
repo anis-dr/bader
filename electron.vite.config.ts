f86771aceb2110fc1c9f7103ed4c56c3645f6fe4
Anis Lazaar <anis00723@gmail.com>
2025-02-10 03:24:08 +0100

    Remove explicit sqlite3 externalization in Electron Vite config

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -4,11 +4,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   main: {
-    plugins: [
-      externalizeDepsPlugin({
-        include: ['sqlite3']
-      })
-    ]
+    plugins: [externalizeDepsPlugin()]
   },
   preload: {
     plugins: [externalizeDepsPlugin()]
